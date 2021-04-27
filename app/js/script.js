4c8777b63fac503ae068a0273683228bd68efa5d
Farrah Fazirah <farrahfazirah@gmail.com>
2021-04-28 01:35:46 +0800

    Fixed navbar issues

--- app/js/script.js
+++ app/js/script.js
@@ -1,82 +1,26 @@
-
-
-// NAVIGATION LOGO SCROLL TOP
-
-$('.logo').on('click', function(e) {
-  e.preventDefault();
-  $('.nav-toggle').removeClass('open');
-  $('.menu-left').removeClass('collapse');
-  $('html, body').animate({
-    scrollTop: 0
-  }, 750, 'easeInOutQuad')
-});
-
-// LINKS TO ANCHORS
-$('a[href^="#"]').on('click', function(event) {
-
-  var $target = $(this.getAttribute('href'));
-
-  if($target.length) {
-    event.preventDefault();
-    $('html, body').stop().animate({
-      scrollTop: $target.offset().top
-    }, 750, 'easeInOutQuad');
-  }
-});
-
-// TOGGLE HAMBURGER & COLLAPSE NAV
-$('.nav-toggle').on('click', function() {
-  $(this).toggleClass('open');
-  $('.menu-left').toggleClass('collapse');
-});
-// REMOVE X & COLLAPSE NAV ON ON CLICK
-$('.menu-left a').on('click', function() {
-  $('.nav-toggle').removeClass('open');
-  $('.menu-left').removeClass('collapse');
-});
-
-// SHOW/HIDE NAV
-
-// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('nav').outerHeight();
-
-$(window).scroll(function(event){
-    didScroll = true;
-});
-
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 250);
-
-function hasScrolled() {
-    var st = $(this).scrollTop();
-
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $('nav').removeClass('show-nav').addClass('hide-nav');
-        $('.nav-toggle').removeClass('open');
-        $('.menu-left').removeClass('collapse');
-    } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('nav').removeClass('hide-nav').addClass('show-nav');
-        }
-    }
-
-    lastScrollTop = st;
-}
+(function($) { // Begin jQuery
+  $(function() { // DOM ready
+    // If a link has a dropdown, add sub menu toggle.
+    $('nav ul li a:not(:only-child)').click(function(e) {
+      $(this).siblings('.nav-dropdown').toggle();
+      // Close one dropdown when selecting another
+      $('.nav-dropdown').not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+    // Clicking away from dropdown will remove the dropdown class
+    $('html').click(function() {
+      $('.nav-dropdown').hide();
+    });
+    // Toggle open and close nav styles on click
+    $('#nav-toggle').click(function() {
+      $('nav ul').slideToggle();
+    });
+    // Hamburger to X toggle
+    $('#nav-toggle').on('click', function() {
+      this.classList.toggle('active');
+    });
+  }); // end DOM ready
+})(jQuery); // end jQuery
 
  // BLOB
   var radius = 8;
